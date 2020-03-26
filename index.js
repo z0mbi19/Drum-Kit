0bc616bea6e11aef3dff979c01270cdf387d7b5e
z0mbi19 <32523746+z0mbi19@users.noreply.github.com>
2020-03-26 11:37:39 -0300

    Que começe o barulho

---
+++ index.js
@@ -0,0 +1,10 @@
+for (var i = 0; i <= document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+    this.style.color = 'white'
+  })
+}
+
+
+
+//var audio = new Audio("sounds/tom-1.mp3")
+//audio.play()
